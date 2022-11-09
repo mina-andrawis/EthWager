828d6659b569bbff8eb0d399b43b2b6fbf004c57
Mina Andrawis <maa16s@my.fsu.edu>
2022-11-08 23:00:35 -0500

    simple styled table with test data

--- ethwager/src/pages/home.js
+++ ethwager/src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ProjectList from '../components/projectList.js'
+import ProjectList from '../components/projectsTable.js'
 const Home = () =>{
   return (
     <div>
