31c4e57ec2bc1e69b3034bebcb805d7867fa8be7
Mina Andrawis <mi.andrawis@gmail.com>
2022-11-23 22:48:40 -0500

    OpenSea API returning in cosole, throwing error when outputting to dom

--- ethwager/src/components/displayTable.js
+++ ethwager/src/components/displayTable.js
@@ -14,14 +14,19 @@ class DisplayTable extends Component {
   }
 
   componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res => res.json())
+    const options = {method: 'GET', headers: {accept: 'application/json'}};
+
+    fetch('https://api.opensea.io/api/v1/collection/doodles-official/stats', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
     .then(json => {
       this.setState({
         isLoaded:true,
         items: json,
       })
-    });
+    })
+    .catch(err => console.error(err));
+
   }
 
 render() {
@@ -35,11 +40,11 @@ render() {
       return (
         <div className="DisplayTable">
           <ul>
-            {items.map(item => (
-              <li key={item.id}>
-                Name: {item.name} | Email: {item.email}
-              </li>
-            ))}
+            {(items || []).map(item => (
+            <li key={item.id}>
+              Name: {item.count}
+            </li>
+          ))}
           </ul>
 
         </div>
