5a4b500c0462c8c4ac52efb3f86f50e604a119f0
Mina Andrawis <mi.andrawis@gmail.com>
2022-11-27 01:19:36 -0500

    Fixed console error, cant have hook in useEffect()

--- ethwager/src/components/displayTable.js
+++ ethwager/src/components/displayTable.js
@@ -4,28 +4,25 @@ import "../styles/displayTable.css"
 const DisplayTable = () => {
   const [floor, setFloor] = useState("");
 
+  const url = "https://api.opensea.io/api/v1/collection/doodles-official/stats"
+
+  const fetchData = async () => {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setFloor(json.stats.floor_price)
+    } catch (error) {
+        console.log("error", error);
+    }
+  };
+
   useEffect(() => {
-    const url = "https://api.opensea.io/api/v1/collection/doodles-official/stats"
-
-
-    const fetchData = async () => {
-      try {
-          const response = await fetch(url);
-          const json = await response.json();
-          setFloor(json.stats.floor_price)
-      } catch (error) {
-          console.log("error", error);
-      }
-    };
-
-    fetchData();  
+    fetchData(url);  
   }, []);
 
   return (
     <div>{floor}</div>
   );
-
-
 };
 
 export default DisplayTable;
