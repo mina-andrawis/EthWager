42f7a9c2bd47519c7ebf7166d41b5459fe355823
Mina Andrawis <mi.andrawis@gmail.com>
2023-01-04 01:31:44 -0500

    Added useToken hook, token state is not being set for unknown reason
    token in browser storage is being set

--- ethwager/src/pages/my-account.js
+++ ethwager/src/pages/my-account.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react';
 import Login from "../components/login"
+import useToken from "../hooks/useToken"
+
+
 const MyAccount = () =>{
 
-  const [token, setToken] = useState();
+  const { token, setToken } = useToken();
 
   if(!token) {
     return <Login setToken={setToken} />
   }
-
-
+  
   return (
     <div>
       <div>
