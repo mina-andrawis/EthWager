b079f6ae5af745dd8f5a39a03ee84529b8e66ad9
Mina Andrawis <maa16s@my.fsu.edu>
2023-01-11 23:36:23 -0500

    Confirmation component appears after continued is clicked
    necessary data is passed through props

--- ethwager/src/components/bidModal.js
+++ ethwager/src/components/bidModal.js
@@ -22,57 +22,69 @@ const BidModal = (props) => {
     setOpenConfirmationModal(true);
   }
 
+  const handleConfirmationClose = () => {
+    //setOpenConfirmationModal(false);
+  }
+
   if (proj.floor_price)
   {
     return (
-      <>
-        {openConfirmationModal === true  && <ConfirmationModal proj={proj} projName={projName} bid={bid} expiration={expiration} setModal={setModal}/>}
+      <> 
+        {/* pass callback function as handleClose to close the child component from inside ConfirmationModal*/}
+        {openConfirmationModal
+        && <ConfirmationModal 
+        handleClose={handleConfirmationClose} 
+        bid={bid} 
+        projName={projName} 
+        floorprice={proj.floor_price}/>}
 
-        <div className="modalBackground">
-          <div className="modalContainer">
-            <div className="titleCloseBtn">
-                <button onClick={() => { setModal(false) }}> 
-                    X
-                </button>
-            </div>
-            <div className="title">
-                <h1>{projName}</h1>            
-                <h2>The floor price for this project is: {(proj.floor_price)} ETH</h2>
-            </div>
-            <div className="top body">
-                <div className="bidQuestion">
-                    <p>Are you bullish or bearish?</p>
-                </div>
-                <div className="bidButtons">
-                    <button className={bid === 'bullish' ? 'selected fancyButton bullishButton' : 'fancyButton bullishButton'} onClick={() => setBid('bullish')}>
-                      Bullish
-                    </button>
-                    <button className={bid === 'bearish' ? 'selected fancyButton bearishButton' : 'fancyButton bearishButton'} onClick={() => setBid('bearish')}>
-                      Bearish
-                    </button>
-                </div>
-            </div>
-            <div className="bottom body" >
-                <div className="expirationQuestion">
-                    <p>When would you like this wager to expire?</p>
-                </div>
-                <div className="expirationButtons">
-                    <button className={expiration === 15 ? "selected fancyButton expirationButton" : "fancyButton expirationButton"} onClick={() => setExpiration(15)}>
-                      15 days
-                    </button>
-                    <button className={expiration === 30 ? "selected fancyButton expirationButton" : "fancyButton expirationButton"} onClick={() => setExpiration(30)}>
-                      30 days
-                    </button>
-                </div>
-            </div>
-            <div className="footer">
-            <button onClick={() => { setModal(false) }} id="cancelBtn">
-                Cancel
-            </button>
-            <button onClick={handleContinue}>Continue</button>
+        {!openConfirmationModal &&
+          <div className="modalBackground">
+            <div className="modalContainer">
+              <div className="titleCloseBtn">
+                  <button onClick={() => { setModal(false) }}> 
+                      X
+                  </button>
+              </div>
+              <div className="title">
+                  <h1>{projName}</h1>            
+                  <h2>The floor price for this project is: {(proj.floor_price)} ETH</h2>
+              </div>
+              <div className="top body">
+                  <div className="bidQuestion">
+                      <p>Are you bullish or bearish?</p>
+                  </div>
+                  <div className="bidButtons">
+                      <button className={bid === 'bullish' ? 'selected fancyButton bullishButton' : 'fancyButton bullishButton'} onClick={() => setBid('bullish')}>
+                        Bullish
+                      </button>
+                      <button className={bid === 'bearish' ? 'selected fancyButton bearishButton' : 'fancyButton bearishButton'} onClick={() => setBid('bearish')}>
+                        Bearish
+                      </button>
+                  </div>
+              </div>
+              <div className="bottom body" >
+                  <div className="expirationQuestion">
+                      <p>When would you like this wager to expire?</p>
+                  </div>
+                  <div className="expirationButtons">
+                      <button className={expiration === 15 ? "selected fancyButton expirationButton" : "fancyButton expirationButton"} onClick={() => setExpiration(15)}>
+                        15 days
+                      </button>
+                      <button className={expiration === 30 ? "selected fancyButton expirationButton" : "fancyButton expirationButton"} onClick={() => setExpiration(30)}>
+                        30 days
+                      </button>
+                  </div>
+              </div>
+              <div className="footer">
+              <button onClick={() => { setModal(false) }} id="cancelBtn">
+                  Cancel
+              </button>
+              <button onClick={handleContinue}>Continue</button>
+              </div>
             </div>
           </div>
-        </div>
+        } 
       </>
     );
   }
--- ethwager/src/components/confirmationModal.js
+++ ethwager/src/components/confirmationModal.js
@@ -2,31 +2,34 @@ import React, { useState } from "react";
 import "../styles/confirmationModal.css";
 
 const ConfirmationModal = (props) => {
-  const { proj, projName, bid, expiration, setModal } = props;
+  const { handleClose,proj, projName, floorprice} = props;
 
   const handleConfirm = () => {
     // Perform some action with the bid and expiration values here, such as sending a request to a server to create a new wager
-    console.log(`Confirmed bid: ${bid}, expiration: ${expiration} days`);
-    setModal(false);
+    // console.log(`Confirmed bid: ${bid}, expiration: ${expiration} days`);
   };
+
+  //handleClose(false);
+  console.log('hi');
+
 
   return (
     <div className="modalBackground">
       <div className="cMmodalContainer">
         <div className="titleCloseBtn">
-          <button onClick={() => setModal(false)}>X</button>
+          <button onClick={handleClose()}>X</button>
         </div>
         <div className="modal-title">
           <h1>{projName}</h1>
-          {proj.floor_price && (
-            <h2>The floor price for this project is: {proj.floor_price} ETH</h2>
+          {floorprice && (
+            <h2>The floor price for this project is: {floorprice} ETH</h2>
           )}
         </div>
         <div className="modal-body">
-          <p>Are you sure you want to place a {bid} bid with an expiration of {expiration} days?</p>
+          {/* <p>Are you sure you want to place a {bid} bid with an expiration of {expiration} days?</p> */}
         </div>
         <div className="modal-footer">
-          <button onClick={() => setModal(false)}>Cancel</button>
+          <button onClick={handleClose()}>Cancel</button>
           <button onClick={handleConfirm}>Confirm</button>
         </div>
       </div>
