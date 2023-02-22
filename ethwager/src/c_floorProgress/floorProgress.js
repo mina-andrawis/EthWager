ada00dd56a4fc172b4b2960589185e789ebb86e9
Mina Andrawis <maa16s@my.fsu.edu>
2023-02-21 22:24:31 -0500

    Added formatting for Progress table, need to handle overflow

--- ethwager/src/c_floorProgress/floorProgress.js
+++ ethwager/src/c_floorProgress/floorProgress.js
@@ -12,23 +12,42 @@ const FloorProgress = ({ wagerId }) => {
 
   const formatEth = (num) => {
     var formatted = (Math.floor(num * 1000)/1000).toFixed(2).substr(0,4).replace(/\.$/,'');
-    return ( `${formatted} ETH`)
+    return ( `${formatted}  `)
   }
 
   const formatDate = (date) => {
     return (new Date(date)).toLocaleDateString("en-US", {year: '2-digit', month: '2-digit', day: '2-digit'})
   }
 
+
   return (
     <div className={styles.allProgressContainer}>
+      <div className={styles.progressContainer}>
+          <tr className={styles.progress}>
+            <td className={styles.progress}>
+              <span className={styles.progressDate}>
+                {"Date"}
+              </span>
+              <td style={{ height:5 }}></td>
+              <span className={styles.progressEth}>
+              {"Floor Price (ETH)"}
+              </span>
+            </td>
+        </tr> 
+        </div>
       {progressData.floor_data.map((progress, index) => (
         <div key={progress.wagerId} className={styles.progressContainer}>
-          <tr key={`${progress.wagerId}-date`} className={styles.progress}>
-            <td className={styles.progress}>{formatDate(progressData.date_data[index])}</td>
-          </tr>
-          <tr key={`${progress.wagerId}-eth`}>
-            <td className={styles.progress}>{formatEth(progressData.floor_data[index])}</td>
-          </tr>
+          <tr key={`${progress.wagerId}-data`} className={styles.progress}>
+            <td className={styles.progress}>
+              <span className={styles.progressDate}>
+                {formatDate(progressData.date_data[index])}
+              </span>
+              <td style={{ height:5 }}></td>
+              <span className={styles.progressEth}>
+                {formatEth(progressData.floor_data[index])}
+              </span>
+            </td>
+          </tr> 
         </div>
       ))}
     </div>
